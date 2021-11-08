f893565889128abbd41581d38e7798dd3020aaa6
llwslc <llwslc@gmail.com>
2021-11-09 01:12:34 +0800

    Update injectScript.js

--- js/injectScript.js
+++ js/injectScript.js
@@ -1,7 +1,7 @@
 const findCoinRow = () => {
   const coinRow = document.getElementById('MUIDataTableBodyRow-6');
 
-  const changeEl = (parentNode, newValue) => {
+  const changeEl = (parentNode, newValue, balance = false) => {
     parentNode.childNodes[0].style.display = 'none';
 
     const curNewEl = parentNode.childNodes[1];
@@ -10,33 +10,43 @@ const findCoinRow = () => {
         curNewEl.innerText = newValue;
       }
     } else {
-      const newEl = document.createElement('p');
-      newEl.className = 'MuiTypography-root MuiTypography-body2';
-      parentNode.appendChild(newEl);
-      newEl.innerText = newValue;
+      if (balance) {
+        const newEl = document.createElement('b');
+        parentNode.appendChild(newEl);
+        newEl.innerText = newValue;
+      } else {
+        const newEl = document.createElement('p');
+        newEl.className = 'MuiTypography-root MuiTypography-body2';
+        parentNode.appendChild(newEl);
+        newEl.innerText = newValue;
+      }
     }
   };
 
-  if (coinRow) {
-    const times = 189;
+  try {
+    if (coinRow) {
+      const times = 189;
 
-    let balanceCoin = coinRow.childNodes[3].childNodes[0].innerText;
-    balanceCoin = balanceCoin.replace(',', '');
-    balanceCoin = parseFloat(balanceCoin) * times;
+      let balanceCoin = coinRow.childNodes[3].childNodes[0].innerText;
+      balanceCoin = balanceCoin.replace(',', '');
+      balanceCoin = parseFloat(balanceCoin) * times;
 
-    const coinFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 6 });
+      const coinFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 6 });
 
-    changeEl(coinRow.childNodes[3], coinFormat.format(balanceCoin));
-    changeEl(coinRow.childNodes[5], coinFormat.format(balanceCoin));
+      changeEl(coinRow.childNodes[3], coinFormat.format(balanceCoin), true);
+      changeEl(coinRow.childNodes[5], coinFormat.format(balanceCoin));
 
-    let balanceUSD = coinRow.childNodes[7].childNodes[0].innerText;
+      let balanceUSD = coinRow.childNodes[7].childNodes[0].innerText;
 
-    balanceUSD = balanceUSD.replace('US$', '');
-    balanceUSD = parseFloat(balanceUSD) * times;
+      balanceUSD = balanceUSD.replace('US$', '');
+      balanceUSD = parseFloat(balanceUSD) * times;
 
-    const usdFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 });
+      const usdFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 });
 
-    changeEl(coinRow.childNodes[7], `US$${usdFormat.format(balanceUSD)}`);
+      changeEl(coinRow.childNodes[7], `US$${usdFormat.format(balanceUSD)}`);
+    }
+  } catch (error) {
+    console.log(error);
   }
 
   setTimeout(() => {
