608dcc3077b74d1c29f75d71b5cf975c44abad02
llwslc <llwslc@gmail.com>
2022-01-31 15:47:35 +0800

    Update injectScript.js

--- js/injectScript.js
+++ js/injectScript.js
@@ -1,8 +1,24 @@
 const findCoinRow = () => {
-  const coinRow = document.getElementById('MUIDataTableBodyRow-6');
+  const coinRow = document.getElementById('MUIDataTableBodyRow-7');
 
-  const changeEl = (parentNode, newValue, balance = false) => {
+  const changeEl = (parentNode, newValue, { balance = false, total = false } = {}) => {
     parentNode.childNodes[0].style.display = 'none';
+
+    if (total) {
+      const curNewEl = parentNode.childNodes[2];
+      if (curNewEl) {
+        curNewEl.className = 'MuiTypography-root jss299 MuiTypography-h5';
+        if (curNewEl.innerText !== newValue) {
+          curNewEl.innerText = newValue;
+        }
+      } else {
+        const newEl = document.createElement('div');
+        newEl.className = 'MuiTypography-root jss299 MuiTypography-h5';
+        parentNode.appendChild(newEl);
+        newEl.innerText = newValue;
+      }
+      return;
+    }
 
     const curNewEl = parentNode.childNodes[1];
     if (curNewEl) {
@@ -24,8 +40,11 @@ const findCoinRow = () => {
   };
 
   try {
+    let balanceUSD = 0;
+    const usdFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 });
+
     if (coinRow) {
-      const times = 189;
+      const times = 23.5;
 
       let balanceCoin = coinRow.childNodes[3].childNodes[0].innerText;
       balanceCoin = balanceCoin.replace(',', '');
@@ -33,17 +52,27 @@ const findCoinRow = () => {
 
       const coinFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 6 });
 
-      changeEl(coinRow.childNodes[3], coinFormat.format(balanceCoin), true);
+      changeEl(coinRow.childNodes[3], coinFormat.format(balanceCoin), { balance: true });
       changeEl(coinRow.childNodes[5], coinFormat.format(balanceCoin));
 
-      let balanceUSD = coinRow.childNodes[7].childNodes[0].innerText;
+      balanceUSD = coinRow.childNodes[7].childNodes[0].innerText;
 
-      balanceUSD = balanceUSD.replace('US$', '');
+      balanceUSD = balanceUSD.replace('US$', '').replace(',', '');
       balanceUSD = parseFloat(balanceUSD) * times;
 
-      const usdFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2 });
-
       changeEl(coinRow.childNodes[7], `US$${usdFormat.format(balanceUSD)}`);
+    }
+
+    let total = document.getElementsByClassName('jss299')[0];
+
+    if (total) {
+      total = total.parentNode;
+      const linkStr = ' ≈ US$';
+      let totalStrPre = total.childNodes[0].innerText.split(linkStr)[0];
+      let totalStrNum = total.childNodes[0].innerText.split(linkStr)[1];
+      totalStrNum = totalStrNum.replace(',', '');
+      totalStrNum = parseFloat(totalStrNum) + balanceUSD;
+      changeEl(total, `${totalStrPre}${linkStr}${usdFormat.format(totalStrNum)}`, { total: true });
     }
   } catch (error) {
     console.log(error);
